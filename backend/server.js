438a6e593712987f1a76495bedc8452deb953fbe
Arrakis Eswarner <arrakiseswarner@gmail.com>
2024-02-19 14:51:40 +0530

    socket.io part completed

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -20,7 +20,7 @@ app.use("/api/users", userRoutes);
 //app.get("/", (req, res) => {
 //res.send("Hello World!!");
 //});
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
